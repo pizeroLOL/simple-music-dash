4310eeb329c9a32a63211d5662794a33331bfefd
Pizero <zhaory200707@outlook.com>
2025-04-06 02:07:30 +0800

    init: new framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import solidPlugin from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+  plugins: [solidPlugin(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
   },
 })
